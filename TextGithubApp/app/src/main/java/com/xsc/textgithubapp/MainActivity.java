aedf42aa456315be01adf95354a7c755b9bcec26
tongruijun <351219265@qq.com>
2016-05-03 10:03:44 +0800

    添加获取控件的方法

--- TextGithubApp/app/src/main/java/com/xsc/textgithubapp/MainActivity.java
+++ TextGithubApp/app/src/main/java/com/xsc/textgithubapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.xsc.textgithubapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    private Untils untils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        untils = new Untils();
+        initView();
+
+    }
+
+    private void initView() {
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText(untils.getVault());
     }
 }
---
+++ TextGithubApp/app/src/main/java/com/xsc/textgithubapp/Untils.java
@@ -0,0 +1,12 @@
+package com.xsc.textgithubapp;
+
+/**
+ * 作者：小童 on 2016/5/3 0003 09:36
+ * 简介：
+ */
+public class Untils {
+
+    public String getVault() {
+        return "获得值";
+    }
+}
